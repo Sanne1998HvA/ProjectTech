bea19ea5df1328cea38bce63b811878d86172e70
Sanne Kes <sannerosa.kes@gmail.com>
2022-03-14 15:18:39 +0100

    data terug geven in terminal

--- config/db.js
+++ config/db.js
@@ -14,3 +14,25 @@ const connectDB = () => {
   }
  }
  module.exports = connectDB
+
+// // Alles hieronder zorgt voor de validatie van de form
+// app.post("/register", (req, res) =>{
+
+//   //checkt of het ingevoerde email adres niet eerder is gebruikt voor het aanmaken van een account
+
+// User.findOne({ email: req.body.email }).then((user) => {
+//     if  (user) {
+//       // Geef een 400 error als de ingevulde email al bestaat
+//       return res.status(400).json({ email: "Een gebruiker heeft deze email al geregistreerd"})
+//     } else {
+//       // Als deze niet bestaat, maak nieuwe gebruiker aan
+//       const newUser = new User({
+//         userName: req.body.userName,
+//         email: req.body.email,
+//         password: req.body.password,
+//       });
+//         newUser.save()
+//       return res.status(200).json ({msg: newUser})
+//     }
+//   });
+// });
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const {engine} = require('express-handlebars')
+const bodyParser = require('body-parser')
 const app = express()
 // voor de form
 const User = require('./models/User')
@@ -16,6 +17,8 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static(__dirname + '/static'));
 
 app.get('/', (req, res) => {
@@ -31,37 +34,23 @@ app.get('/about', (req, res) => {
     });
   })
 
-  app.get('/signup', (req, res) => {
-    res.render('signup')
-    })
+app.get('/inloggen', (req, res) => {
+  res.render('inloggen')
+      })
+
+app.get('/signup', (req, res) => {
+  res.render('signup')
+      })
+
+app.post ("/inloggen", (req, res) => {
+  console.log (req.body)
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on localhost:${port}`)
 })
 
 
-
-
-// Alles hieronder zorgt voor de validatie van de form
-app.post("/register", (req, res) =>{
-  //checkt of het ingevoerde email adres niet eerder is gebruikt voor het aanmaken van een account
-
-User.findOne({ email: req.body.email }).then((user) => {
-    if  (user) {
-      // Geef een 400 error als de ingevulde email al bestaat
-      return res.status(400).json({ email: "Een gebruiker heeft deze email al geregistreerd"})
-    } else {
-      // Als deze niet bestaat, maak nieuwe gebruiker aan
-      const newUser = new User({
-        userName: req.body.userName,
-        email: req.body.email,
-        password: req.body.password,
-      });
-        newUser.save()
-      return res.status(200).json ({msg: newUser})
-    }
-  });
-});
 
 
 
