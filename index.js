851cbb0b680f1b892eda42aa61a681c219684739
Sanne Kes <sannerosa.kes@gmail.com>
2022-03-15 10:48:20 +0100

    code update inloggen registreren

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const port = process.env.port || 1337
 require('dotenv').config()
 
 const connectDB = require('./config/db')
+const res = require('express/lib/response')
 connectDB();
 
 
@@ -20,6 +21,8 @@ app.set('views', './views');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + '/static'));
+
+app.use(express.urlencoded({ extented: true}));
 
 app.get('/', (req, res) => {
    res.render('home')
@@ -45,6 +48,72 @@ app.get('/signup', (req, res) => {
 app.post ("/inloggen", (req, res) => {
   console.log (req.body)
   })
+
+app.post('/home', async ( req, res) => {
+  const gebruikersnaam = req.body.gebruikersnaaam
+  const wachtwoord = req.body.wachtwoord
+  try {
+    const verborgenWachtwoord = await bcrypt.hash(password, 10)
+  
+  const result = await User.create({
+    gebruikersnaaam: userName,
+    email: email,
+    wachtwwoord: verborgenWachtwoord 
+  })
+    res.redirect('/about')
+  } catch {
+    console.log('Niet gelukt om in te loggen, probeer het nog eens.')
+    res.redirect('home')
+  }
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+      person: {
+      gebruikersnaam: user,
+    }
+    })
+  })
+
+
+
+
+
+
+
+
+
+// Alles hieronder zorgt voor de validatie van de form
+  
+// //checkt of het ingevoerde email adres niet eerder is gebruikt voor het aanmaken van een account
+// app.post("/register", (req, res) => {
+
+//     User.findOne({ email: req.body.email }).then((user) => {
+//         if  (user) {
+//           // Geef een 400 error als de ingevulde email al bestaat
+//           return res.status(400).json({ email: "Een gebruiker heeft deze email al geregistreerd"})
+//         } else {
+//           // Als deze niet bestaat, maak nieuwe gebruiker aan
+//           const newUser = new User({
+//             userName: req.body.userName,
+//             email: req.body.email,
+//             password: req.body.password,
+//           });
+//             newUser.save()
+//           return res.status(200).json({msg: newUser})
+//         }
+//       });
+//     });
+    
+app.post('/post-feedback', function (req, res) {
+    dbConn.then(function(db) {
+        delete req.body._id; // for safety reasons
+        db.collection('feedbacks').insertOne(req.body);
+      });
+
+      res.send('Data received:\n' + JSON.stringify(req.body));
+  });
+      
 
 app.listen(port, () => {
   console.log(`Example app listening on localhost:${port}`)
--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const UserSchema = new Schema ({
     userName: {
@@ -14,11 +14,7 @@ const UserSchema = new Schema ({
     password: {
         type: String,
         required: true,
-    },
-    date: {
-        type: Date,
-        defauld: Date.now,
-    },
+    }
 });
 
 module.exports = User = mongoose.model("User", UserSchema);
